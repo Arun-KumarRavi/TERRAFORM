453b60433921d92d42634e5d851099c9101af07f
Arun-KumarRavi <arunkumarravi0811@gmail.com>
2025-12-20 00:35:35 +0530

    Initial commit: EC2 modules and folder structure

---
+++ AWS-TERRAFORM/Aws-ec2-module-12/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0ecb62995f68bb549"   
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Myec2"
+  }
+}
