080fb717da569c3b975bce7f67e1733feb43c356
Arun-KumarRavi <arunkumarravi0811@gmail.com>
2025-12-20 00:42:06 +0530

    resource changed

--- AWS-TERRAFORM/Ec2-modify-module-13/main.tf
+++ AWS-TERRAFORM/Ec2-modify-module-13/main.tf
@@ -13,9 +13,9 @@ provider "aws" {
 
 resource "aws_instance" "myec2" {
   ami           = "ami-0ecb62995f68bb549"   
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
-    Name = "Myec2"
+    Name = "Myec2-resource-change"
   }
 }
